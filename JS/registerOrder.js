8ec160d5b52159492066eeaa27fa2c56dd44f12d
Parsa Emami <parsaemami65@gamil.com>
2025-05-02 00:02:58 +0330

    the invoice is designed and dynamic  --- alpha version --- address doesnt work

---
+++ JS/registerOrder.js
@@ -0,0 +1,6 @@
+const printbtn = document.querySelector("#printbtn");
+printbtn.addEventListener("click", () => {
+    print();
+});
+
+alert("سفارش شما با موفقیت ثبت شد !")
--- php/registerOrder.php
+++ php/registerOrder.php
@@ -1,33 +1,44 @@
 <?php
-// if (isset($_POST['user_realname']) && isset($_POST['username']) && isset($_POST['postal_code']) && isset($_POST['phoneNumber']) && isset($_POST['pro_code'])) {
-//     $user_realname = $_POST['user_realname'];
-//     $username = $_POST['username'];
-//     $postal_code = $_POST['postal_code'];
-//     $phoneNumber = $_POST['phoneNumber'];
-//     $pro_code = $_POST['pro_code'];
-// }
+if (isset($_POST['user_realname']) && isset($_POST['username']) && isset($_POST['postal_code']) && isset($_POST['phoneNumber']) && isset($_POST['pro_code'])) {
+    $user_realname = $_POST['user_realname'];
+    $username = $_POST['username'];
+    $postal_code = $_POST['postal_code'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $pro_code = $_POST['pro_code'];
+}
 
-// $link = mysqli_connect(hostname: "localhost", username: "root", password: "", database: "car_shop");
-// $queryReadInfo = "SELECT * from products WHERE pro_code = '$pro_code'";
-// $resultReadInfo = mysqli_query($link, $queryReadInfo);
-// if ($resultReadInfo && mysqli_num_rows($resultReadInfo) > 0) {
-//     $row = mysqli_fetch_assoc($resultReadInfo);
-//     $title = $row['title'];
-//     $price = $row['price'];
-//     $discount = $row['discount'];
-// }
-// $queryInsertInfo = "INSERT INTO orders (user_realname,username,user_phoneNumber,postal_code,pro_code,pro_name,price,discount) VALUES 
-// ('$user_realname','$username','$phoneNumber','$postal_code','$pro_code','$title','$price','$discount')";
-// $result = mysqli_query($link, $queryInsertInfo);
-// if ($result == true) {
-//     echo "the information is registered successfully";
-// }
+$link = mysqli_connect(hostname: "localhost", username: "root", password: "", database: "car_shop");
+$queryReadInfo = "SELECT * from products WHERE pro_code = '$pro_code'";
+$resultReadInfo = mysqli_query($link, $queryReadInfo);
+if ($resultReadInfo && mysqli_num_rows($resultReadInfo) > 0) {
+    $row = mysqli_fetch_assoc($resultReadInfo);
+    $title = $row['title'];
+    $price = $row['price'];
+    $discount = $row['discount'];
+}
+$queryInsertInfo = "INSERT INTO orders (user_realname,username,user_phoneNumber,postal_code,pro_code,pro_name,price,discount) VALUES 
+('$user_realname','$username','$phoneNumber','$postal_code','$pro_code','$title','$price','$discount')";
+$result = mysqli_query($link, $queryInsertInfo);
+if ($result == true) {
+    echo "the information is registered successfully";
+}
 
-// if ($link == false) {
-//     echo "could't connect to the database !";
-// } else {
-//     echo "connected to the database";
-// }
+if ($link == false) {
+    echo "could't connect to the database !";
+} else {
+    echo "connected to the database";
+}
+
+function calcPrice($price,$discount) {
+    if ($discount != 0) {
+        $mainPrice = $price;
+        $discountPercent = $discount;
+        $finalPrice = $mainPrice - (($discountPercent / 100) * $mainPrice);
+        return $finalPrice;
+    } else {
+        return $price;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +49,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>فاکتور</title>
     <link rel="stylesheet" href="../css/main.css" type="text/css">
+    <script src="../JS/registerOrder.js" defer></script>
 </head>
 
 <body>
@@ -80,32 +92,46 @@
                 </div>
                 <div class="bottom_part">
                 <div class="items" id="pro_code">
-                        <p>21</p>
+                        <p><?php echo $pro_code; ?></p>
                     </div>
                     <div class="items" id="pro_name">
-                        <p>بی ام و 2019 مدل i320</p>
+                        <p><?php echo $title; ?></p>
                     </div>
                     <div class="items" id="price">
-                        <p>5,700,000,000</p>
+                        <p><?php echo number_format($price); ?></p>
                     </div>
                     <div class="items" id="discount">
-                        <p>17%</p>
+                        <p><?php echo $discount; ?></p>
                     </div>
                     <div class="items" id="discounted">
-                        <p>3,700,000,000</p>
+                        <p><?php echo number_format(calcPrice($price,$discount)) ?></p>
                     </div>
                     <div class="items" id="user_realname">
-                        <p>پارسا امامی</p>
+                        <p><?php echo $user_realname; ?></p>
                     </div>
                     <div class="items" id="username">
-                        <p>cloner</p>
+                        <p><?php echo $username; ?></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="address_container">
-        <div class="addressBox"></div>
+        <div class="title">
+            <p>آدرس :</p>
+        </div>
+        <div class="addressBox">
+            <div class="addressField">
+                <p>میدان شهدا خیابان 17 شهریور میدان شکوفه خیابان غضائری خیابان عامری کوچه اسپندی کوچه احسانی پلاک 4 واحد 6</p>
+            </div>
+        </div>
+        <button id="printbtn">پرینت</button>
+        <div class="postal_code">
+            <p>کد پستی :</p>
+        </div>
+        <div class="postal_code_box">
+            <p><?php echo $postal_code; ?></p>
+        </div>
     </div>
 </body>
 
